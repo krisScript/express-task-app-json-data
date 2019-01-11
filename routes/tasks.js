9f0ba1d576c39bffa12427f174a83c6d49c8c660
Kristiyan Todorov <45337186+krisScript@users.noreply.github.com>
2019-01-11 12:18:34 +0200

    Removing commented code

--- routes/tasks.js
+++ routes/tasks.js
@@ -6,7 +6,7 @@ const tasks = require('../controllers/tasks');
 
 const router = express.Router();
 
-// /admin/add-product => GET
+
 router.get('/tasks/:taskId', tasks.getTask);
 
 router.get('/add-task', tasks.getAddTask);
@@ -22,16 +22,6 @@ router.get('/', tasks.getTasks);
 
 
 
-// /admin/products => GET
-// router.get('/products', tasks.getProducts);
 
-// // /admin/add-product => POST
-// router.post('/add-product', adminController.postAddProduct);
-
-// router.get('/edit-product/:productId', adminController.getEditProduct);
-
-// router.post('/edit-product', adminController.postEditProduct);
-
-// router.post('/delete-product', adminController.postDeleteProduct);
 
  module.exports = router;
